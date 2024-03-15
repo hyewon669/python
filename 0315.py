278065113a96e41b660f224299721f2189278bfa
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2024-03-15 18:47:46 +0900

    Add files via upload

---
+++ 0315.py
@@ -0,0 +1,57 @@
+##print("%-10s" % "hello")
+##print("%10.2f" % 3.141592653)
+
+##name="홍길동"
+##print("내 이름은 {0}이고,{1}월에 태어났다.".format(name,5))
+##name = input("이름: ")
+##high = input("키: ")
+##old = input("나이: ")
+##print("== 자기 소개하기 ==")
+##print(" 나이:  %s" %old)
+##print(" 키: %s" %high)
+##print(" 이름:  %s" %name)
+
+
+##a=input()
+##b=input()
+##a= int(a)
+##b= int(b)
+##print("%d + %d = %d" % (a, b, a+b))
+
+##a= int(input())
+##b=int(input())
+##
+##print("%d %% %d = %d" % (a, b, a%b))
+##print("%d ** %d = %d" % (a, b, a**b))
+
+##a = int(input("a 정수 입력 : "))
+##b = int(input("b 정수 입력 : "))
+##c = int(input("c 정수 입력 : "))
+##print("세 정수의 평균은 %0.2f입니다."%((a+b+c)/3))
+
+##print("%10s""%10d""%10.2lf" %("cat",3,3.14))
+##print("%10s""%10d""%10.3lf" %("dog",127,1.234))
+##print("%10s""%10d""%10.1lf" %("turtle",54321,10.1))
+
+##year=int(input("연도:"))
+##month=int(input("월:"))
+##day=int(input("일:"))
+##print("%d년 %d월 %d일"%(year,month,day))
+
+##L=int(input("L 판매 개수: "))
+##M=int(input("M 판매 개수:"))
+##S=int(input("S 판매 개수:"))
+##sum = 2500 * L
+##sum += 1000 * M
+##sum += 500 * S
+##print("오늘의 총 매출은 %d원입니다." %sum)
+
+##height=int(input("높이: "))
+##width=int(input("밑변:"))
+##area=height*width/2
+##print("area = %.1lf"%(area))
+
+a=int(input("a: "))
+b=int(input("b: "))
+i=a**b
+print("%d"%(i))
