dfebafb3565304f2fee3b12166901ab73d341555
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2024-04-05 18:46:19 +0900

    Add files via upload

---
+++ 0405.py
@@ -0,0 +1,62 @@
+##a='Python'
+##b='is fun'
+##print(a+b)
+##print(a*2)
+
+##a="Carpe Dime!"
+##print(a[6:11])
+
+##a="Carpe Diem!"
+##print(a[6 :])
+
+##a="Carpe Diem!"
+##print(a[-5:-1])
+
+##a="Carpe Diem!"
+##print(a[0:5])
+
+##a='I Love You'
+##print(a.count('o'))
+##print(a.find('Y'), a.index('Y'))
+##print('.'.join(a))
+##print(a.split())
+##print(a.replace('Love','Like'))
+##print(a.upper(),a.lower())
+
+##a="'Hello World'"
+##print(a.count('l'))
+
+##a="phython"
+##print(a.find('p'))
+
+##a="oh!my!god"
+##print(a.split('!'))
+
+##num=input('주민번호: ')
+##a=num[:6]
+##b =num[7]
+##print('생년월일 : %s' % a)
+##print('성별 : %s' % b)
+
+##num=input('주민번호: ')
+##a=num[2:4]
+##b=num[4:6]
+##print('%s월 %s일' % (a,b))
+
+##a="간장공장공장장"
+##print(a.count('공장'))
+##print(a.index('공장'))
+
+##a=['c','e','a','d','b']
+##a.sort()
+##a.remove(a[4])
+##print(a)
+
+list=[]
+a=input('1:')
+b=input('2:')
+c=input('3:')
+list.append(a)
+list.append(b)
+list.append(c)
+print(list)
