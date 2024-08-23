806832997fedc85b6aa3bf3b94552140d985a839
hyewon669 <144100389+hyewon669@users.noreply.github.com>
2024-08-23 18:27:42 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import*
+from court import*
+from ball import*
+win=Tk()
+width=745
+height=374
+win.title("Tennis Game")
+win.geometry("745x374+150+150")
+win.resizable(False,False)
+court=Court(win,width,height,"court.png")
+
+x1,y1=width/2,height/2
+x2,y2=x1+30,y1+30
+ball=Ball(court,x1,y1,x2,y2)
+
+def play_game():
+     ball.move_ball()
+
+     win.after(50,play_game)
+
+play_game()
+win.mainloop()
